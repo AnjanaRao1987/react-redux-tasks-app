52af0ad26a021feaa8f87ba5b40f433508f3dee4
Anjana Gundurao <anjana.eco@gmail.com>
2018-12-13 16:08:33 +0000

    moved folders

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -4,18 +4,20 @@ import { Link } from "react-router-dom";
 
 import Add from '../container/Add';
 import Actions from '../container/Actions';
+import Input from "./Forms/Input";
 
 class Tasks extends Component {
     componentDidMount() {
         this.props.onLoad();
     }
 
+
     render () {
         const { tasks } = this.props;
         return (
             <React.Fragment>
             <Add />
-            <ul className="list-group mx-auto justify-content-center" style={{width:'50%'}}>
+            <ul className="list-group mx-auto justify-content-center" style={{width:'100%'}}>
                 { tasks.map(task => (
                     <li className="list-group-item" key={ task.id } style={{backgroundColor:task.completed?'#E8F7DC':'#EED69D'}}>
                         { /* link to the article using its id */ }
